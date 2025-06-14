d216610911a9e89fa9e9d7af0515f61fc48f1b41
ArtRiv <arthurfelaco707@gmail.com>
2025-06-14 03:34:16 -0300

    chore: remove unused controller and service from AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
@@ -9,7 +7,5 @@ import { UsersModule } from './users/users.module';
   imports: [ConfigModule.forRoot({
     envFilePath: '.env',
   }), AuthModule, UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
